358677631c92662f8a7d05143461858ff21ec0a8
yinjie <yinjie@jusfoun.com>
2019-04-15 16:46:06 +0800

    redis测试代码 demo 简版

---
+++ demo/src/main/java/com/example/demo/RedisApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+
+@SpringBootApplication
+public class RedisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedisApplication.class, args);
+    }
+
+}
---
+++ demo/src/main/java/com/example/demo/controller/TestController.java
@@ -0,0 +1,23 @@
+package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.Serializable;
+
+@RestController
+@RequestMapping("test")
+public class TestController{
+    @Autowired
+    private RedisTemplate redisTemplate;
+    @GetMapping("test")
+    public void send(){
+       redisTemplate.opsForValue().set("abc","AAAAAAAAAAAAAA");
+        ValueOperations<Serializable,String> valueOperations = redisTemplate.opsForValue();
+        System.out.println(redisTemplate.opsForValue().get("dnf"));
+    }
+}
